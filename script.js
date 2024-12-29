292468ecdc20356f59981494dacedd33f782101b
Riman Das <34216663+rimanz@users.noreply.github.com>
2024-12-29 12:27:55 +0600

    Add a way to add current date to the date label

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 const label = document.querySelector('.label');
 const labelText = document.querySelector('.label-text');
 const labelDate = document.querySelector('.label-date');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
+const currentDate = document.querySelector('.curr-date');
+const prevBtn = document.querySelector('.prev-date');
+const nextBtn = document.querySelector('.next-date');
+
+// Initialize Elements:
+const date = new Date();
+currentDate.textContent = date.toLocaleDateString().replaceAll('/', '-');
