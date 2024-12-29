4a3ee93085928223402d5e80d50b13c91ef8dae8
Riman Das <34216663+rimanz@users.noreply.github.com>
2024-12-29 11:39:57 +0600

    Add necessary DOM elements with minimal styles

---
+++ script.js
@@ -0,0 +1,6 @@
+// Elements:
+const label = document.querySelector('.label');
+const labelText = document.querySelector('.label-text');
+const labelDate = document.querySelector('.label-date');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
