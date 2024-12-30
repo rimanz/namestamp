4cd4dbf836437fcce5876117065434a0bf54c99a
Riman Das <34216663+rimanz@users.noreply.github.com>
2024-12-30 12:45:03 +0600

    Add responsiveness to the font size of `.label`
    This makes text-content of `.label-text` to fit into one line regardless
    of it's width.

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ const nextBtn = document.querySelector('.next-date');
 // Initialize Elements:
 const date = new Date();
 printDate();
+changeLabelFontSize();
 
 // Event Listeners:
 prevBtn.addEventListener('click', goPreviousDate);
 nextBtn.addEventListener('click', goNextDate);
+labelText.addEventListener('focusout', changeLabelFontSize);
+labelText.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    changeLabelFontSize();
+  }
+});
 
 // Function declarations:
 function printDate() {
@@ -30,4 +38,15 @@ function goPreviousDate() {
 function goNextDate() {
   date.setDate(date.getDate() + 1);
   printDate();
+}
+
+function calculateFontSize(text) {
+    const availableWidth = labelText.offsetWidth;
+    const fontSize = Math.round(availableWidth / text.length);
+    console.log(availableWidth, fontSize);
+    return fontSize;
+}
+
+function changeLabelFontSize() {
+  label.style.fontSize = calculateFontSize(labelText.textContent) + 'px';
 }
